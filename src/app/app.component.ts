ff38b6128665095614eaf9f933125ed05171adda
Damaris Quiroz <40650579+damarisq-2576@users.noreply.github.com>
2020-06-11 07:22:03 -0300

    restructuring (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
-import { TextService } from './example/_services/text.service';
+import { TextService } from './_services/text.service';
 import { first } from 'rxjs/operators';
 
 
@@ -30,10 +30,7 @@ export class AppComponent {
   constructor(private formBuilder: FormBuilder, private textService: TextService, ) { }
 
   ngOnInit() {
-
-
     this.maiText = this.textService.getMockText();
-
     this.form = this.formBuilder.group({
       signature: [this.maiText, Validators.required]
     });
@@ -75,7 +72,6 @@ export class AppComponent {
 
   setDefinition(getWord){
     const arr :{}[] = [];
-console.log('this.setDefinition')
     this.textService.getDefinition(getWord)
     .pipe(first())
     .subscribe(
@@ -85,27 +81,18 @@ console.log('this.setDefinition')
             word: getWord,
             definition: data[0].defs
           });
-
-
         },
         error(msg) {
           console.log('Error Getting Location: ', msg);
         }
       }
     );
-
     this.defObj = arr;
-console.log(this.defObj)
-
   }
 
   fillTooltip(getWords) {
-
-
     const arr :{}[] = [];
-
     getWords.forEach(el => {
-
       this.textService.getDefinition(el.word)
       .pipe(first())
       .subscribe(
@@ -121,16 +108,14 @@ console.log(this.defObj)
           }
         }
       );
-
-
     });
-    this.defArray = arr;
-    console.log(this.defArray, 'todo')
 
+    this.defArray = arr;
   }
+
   clearSynon() {
     this.showSynon = false;
-    this.errorMsg = "No Synonyms fund";
+    this.errorMsg = "No Synonyms found";
   }
 
 }
